c2c12a4af370cf0f5eea7ef68de5b14675efeef4
tammy <tammylam0@gmail.com>
2020-09-08 14:39:03 -0500

    Updates

---
+++ precache-manifest.6dcd5453edcf2d3aae5cd4b7c84c2566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee79afc79a981d9c8f6dde6d0c11b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e375fff41abf8525ce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3813f7ae890703e390e",
+    "url": "/static/js/2.8d5a2f56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d5a2f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e375fff41abf8525ce",
+    "url": "/static/js/main.d4c0a565.chunk.js"
+  },
+  {
+    "revision": "cd699cd6e80db16b59b7",
+    "url": "/static/js/runtime-main.60b5f66a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
